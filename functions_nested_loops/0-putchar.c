695e5d639aa9e2dc5eed7a016aad31d01d12c5b6
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-06-25 09:02:46 +0000

    Prints putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,26 +1,8 @@
-
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <unistd.h>
 
-int _putchar(char c)
-{
-	return writ(1, &c, 1)
-}
+int _putchar(char c);
 
-
-int main(void)
-
-{
-	_puthcar('_')
-	_puthcar('p')
-	_puthcar('u')
-	_puthcar('t')
-	_puthcar('c')
-	_puthcar('h')
-	_puthcar('a')
-	_puthcar('r')
-
-	return(0);
-}
+#endif
