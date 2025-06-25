e15ca452e6c09d5bcbd9f2d8a1f2875db5ba1e22
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-06-25 09:13:24 +0000

    Prints putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,8 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-int _putchar(char c);
-
-#endif
+	return (0);
+}
