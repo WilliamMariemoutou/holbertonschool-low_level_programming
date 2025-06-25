32fa86b4a4819d81431a7c1c9dd49d86e845f1b2
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-06-25 08:56:13 +0000

    Prints _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+int _putchar(char c)
+{
+	return writ(1, &c, 1)
+}
+
+
+int main(void)
+
+{
+	_puthcar('_')
+	_puthcar('p')
+	_puthcar('u')
+	_puthcar('t')
+	_puthcar('c')
+	_puthcar('h')
+	_puthcar('a')
+	_puthcar('r')
+
+	return(0);
+}
