3dc9b25c3655a8761d8f707a0717e099b352d499
WilliamMariemoutou <williammariemoutou04@gmail.com>
2025-07-01 16:54:48 +0000

    Main

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_most_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+
+#endif /* MAIN_H */
