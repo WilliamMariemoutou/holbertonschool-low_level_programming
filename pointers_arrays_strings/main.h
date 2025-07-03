7beb78090b34ddd9b177c79817512e971e8305ac
WilliamMariemoutou <Williammariemooutou04@gmail.com>
2025-07-03 10:38:52 +0000

    Main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@ void puts2(char *str);
 int _putchar(char c);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char 8dest, char *src);
+char *_strcpy(char *dest, char *src);
 
 
 #endif /* MAIN_H */
