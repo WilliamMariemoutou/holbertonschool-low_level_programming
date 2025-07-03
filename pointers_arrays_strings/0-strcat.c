04acdd07ffd59bb59958771455d64e59ccdc40be
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-07-03 16:25:53 +0000

    concatenates two strings.

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -29,15 +29,3 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
-Update main.h
-c
-Copy
-Edit
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-/* add any other prototypes below */
-
-#endif /* MAIN_H */
