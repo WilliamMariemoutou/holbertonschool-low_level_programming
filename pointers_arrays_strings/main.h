bd2a529f4ec3b49c0e2a5e91a78e0e69c802e3be
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-07-03 07:16:00 +0000

    Main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 void reset_to_98(int *n);
-void swap_int(int *a, int*b);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
 int _putchar(char c);
 
-
-#endif /*MAIN_H*/
+#endif /* MAIN_H */
