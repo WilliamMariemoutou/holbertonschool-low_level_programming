34fe9fa650991d57ae69b0891a62359fb1f281ad
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-07-04 06:39:37 +0000

    Main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,7 +5,8 @@ int _putchar(char c);
 char *_strcat(char *dest, char *src);
 /* add any other prototypes below */
 char *_strncat(char *dest, char *src, int n);
-
+int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
 
 
 
