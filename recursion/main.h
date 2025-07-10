494e39e43e849c924696f2ed0650b716e2691aa9
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-07-10 12:38:15 +0400

    Main

--- recursion/main.h
+++ recursion/main.h
@@ -10,6 +10,11 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
-
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+char *_memset(char *s, char b, unsigned int n);
+int factorial(int n);
 
 #endif /* MAIN_H */
