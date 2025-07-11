646f066c75561a407dfabd1cd7792c0588d8eeda
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-07-11 13:17:43 +0400

    prints its name, followed by a new line.

---
+++ argc_argv/0--whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - prints the name of the program
+*@argc: number of command line arguments (not used)
+*@argv: array of command line arguments
+*
+*Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+(void)argc;
+
+printf("%\n", argv[0]);
+return(0)
+}
