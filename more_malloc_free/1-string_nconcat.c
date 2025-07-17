1d39850f70417db2bc7463d0252b18dbc29d183b
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-07-17 22:14:54 +0400

    concatenates two strings.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 
-	new_str = malloc(sizeof(vhar) * (len1 + n + 1));
+	new_str = malloc(sizeof(char) * (len1 + n + 1));
 	if (new_str == NULL)
 		return (NULL);
 
@@ -49,7 +49,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	new_str[i + j] = '\0';
 
-	retunr (new_str);
+	return (new_str);
 }
 
 
