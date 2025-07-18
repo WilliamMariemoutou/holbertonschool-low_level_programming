ba197b89f8e05ff01ef160ec71da2910a9e6ca07
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-07-18 20:53:44 +0400

    Dogh

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -32,4 +32,10 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner); /* ← This is the prototype */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 #endif
