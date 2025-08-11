ee657774ff28a453247c7d54405ca11afceb601d
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-08-11 14:18:11 +0400

    searches for an integer

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,17 +3,9 @@
 
 #include <stddef.h>
 
-/**
- * array_iterator - executes a function on each element of an array
- * @array: pointer to the array
- * @size: number of elements in the array
- * @action: pointer to the function to be used
- *
- * Description: Applies the function pointed to by @action
- * on each element of @array.
- */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+int int_index(int *array, int size, int (*cmp)(int));
 
 int _putchar(char c);
 
--- function_pointers/main_0.c
+++ function_pointers/main_0.c
@@ -1,11 +1,24 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
+/**
+ * print_elem - prints an integer followed by a new line
+ * @elem: the integer to print
+ *
+ * Description: Prints the integer value of elem to stdout.
+ */
 void print_elem(int elem)
 {
 	printf("%d\n", elem);
 }
 
+/**
+ * main - entry point to test array_iterator
+ *
+ * Description: Creates an array and passes it to array_iterator with print_elem.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int array[5] = {0, 98, 402, 1024, 4096};
