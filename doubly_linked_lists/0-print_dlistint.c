e03488d23c0878758a47fd162511a8732a784d9b
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-08-23 11:20:57 +0400

    Prints all elements of the list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -13,13 +13,13 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 
 	while (h != NULL)
-	{
+	
 		if (h->n == '\0')
 		{
 			printf("%d\n", h->n);
 			count++;
 			h = h->next;
 		}
+	return count;
+}
 
-		return count;
-	}
