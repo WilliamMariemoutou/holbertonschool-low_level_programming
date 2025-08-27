2a39207304947e0d6f0edbbad4d536934dd8175c
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-08-27 14:08:46 +0400

    Adds an element to the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,6 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *current;
 	hash_node_t *new_node;
 	if (ht == NULL || key == NULL || key[0] == '\0')
+
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -27,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(current->key, key) == 0)
 		{
 			free(current->value);
-			current-> value = strdup(value);
+			current->value = strdup(value);
 			return (1);
 		}
 		current = current->next;
