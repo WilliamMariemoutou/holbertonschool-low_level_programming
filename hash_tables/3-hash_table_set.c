af8527971ca3cb5475f69b7720af424a77de7499
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-08-27 14:10:11 +0400

    Adds an element to the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,6 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *current;
 	hash_node_t *new_node;
+
 	if (ht == NULL || key == NULL || key[0] == '\0')
 
 		return (0);
@@ -35,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
-	if(!new_node)
+	if (!new_node)
 		return (0);
 
 	new_node->key = strdup(key);
